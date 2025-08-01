2e8667ce36b12e045243d80b2f4398f25193f52d
tommaso-mandorino <tommasomandorino@outlook.it>
2025-08-01 17:50:01 +0200

    feat(step 3): connect express app to the database and check if everything works

--- server.js
+++ server.js
@@ -1,15 +1,42 @@
 import express from 'express';
+import mysql2 from 'mysql2';
+
+
 
 const SERVER_ADDRESS = process.env.SERVER_ADDRESS;
 const SERVER_PORT = process.env.SERVER_PORT;
 
+
+
 const server = express();
+
+
+
+const mysqlConnectionInformation = {
+    user: 'root',
+    password: 'root',
+    database: 'webapp-express'
+}
+
+const mysqlConnection = new mysql2.createConnection(mysqlConnectionInformation);
+
+if (!mysqlConnection)
+    throw new Error('🚨 Unable to connect to the database.');
+    
+console.log('✅ Connected successfully to the database.');
+
+
 
 server.get('/',
 
     (request, response) => {
 
-        console.log(`Someone visited the 'root' route.`);
+        let clientIp = request.ip;
+
+        if (clientIp.startsWith('::ffff:'))
+            clientIp = clientIp.replace('::ffff:', '');
+
+        console.log(`➕ New visit to the 'root' route from IP: ${clientIp}.`);
 
         response.send('Welcome to the server!');
 
@@ -17,11 +44,13 @@ server.get('/',
 
 );
 
+
+
 server.listen(SERVER_PORT,
 
     () => {
 
-        console.log(`Server is listening on ${SERVER_ADDRESS}:${SERVER_PORT}...`);
+        console.log(`⏳ Server is listening on ${SERVER_ADDRESS}:${SERVER_PORT}...`);
 
     }
 
